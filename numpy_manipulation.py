aa0dc413b0f9ab04bd3fc6366b5c0b0d1e0b29e1
Pedro Elias <epedro520@gmail.com>
2020-05-04 08:21:20 -0300

    Numpy manipulations

---
+++ numpy_manipulation.py
@@ -0,0 +1,48 @@
+import numpy as np 
+
+x = np.array([1,2,3])
+y = np.array([1,2,3])
+print(x+y)
+
+print("\n")
+
+zero_array = np.zeros(5)
+one_array = np.ones(5)
+print(one_array)
+
+print("\n")
+
+z = np.array([[1,2,3],[3,4,5]])
+b = np.array([[1,2,3]]*10)
+print(z)
+
+print("\n")
+
+zeros = np.zeros((5,5))
+print(zeros)
+
+print("\n")
+
+eye_array = np.eye(5)
+print(eye_array)
+
+print("\n")
+
+arrange_array = np.arange(5,10,2)
+print(arrange_array)
+
+print("\n")
+
+matriz1 = np.array([[2,2,2]]*3)
+matriz2 = np.array([[1,2,3]]*3)
+
+print(matriz1+matriz2) 
+
+print("\n")
+
+matriz3 = np.array([[2,2,2]]*3)
+matriz4 = np.array([[1,2,3]]*3)
+print(matriz3*matriz4)
+
+print("\n")
+
