18b7f6974f294a7f762d61712050d2ba42cee0df
Pedro Elias <epedro520@gmail.com>
2020-08-25 09:35:57 -0300

    upgrade: data scalling applied to this file using a dataframe

--- escalonamento.py
+++ escalonamento.py
@@ -1,9 +1,15 @@
 import pandas as pd
 from numpy import * 
-from sklearn import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 
 dados  = pd.read_csv('AppleStore.csv')
-atributos_previsores = dados.iloc[:,0:14]
-atributo_meta = dados.iloc[:,14]
+atributos_previsores = dados.iloc[:,6:10]
+atributo_meta = dados.iloc[:,2]
 
-onehots
+
+
+onehots = pd.get_dummies(data=atributos_previsores,columns=['rating_count_tot','rating_count_ver','user_rating','user_rating_ver'])
+atributos_previsores = onehots.values 
+
+scaler = StandardScaler()
+atributos_previsores_transformed = scaler.fit_transform(atributos_previsores)
